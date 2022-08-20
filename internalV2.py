41a3418efede1676ab574118212387d72cc1058a
varusUbuntu <xushuo@highlight.mobi>
2022-08-21 01:28:42 +0800

    [feature] Add button position

--- internalV2.py
+++ internalV2.py
@@ -2,13 +2,14 @@ import tkinter
 
 button_width: int = 7
 button_height: int = 1
+windows_width = 500
+windows_height = 314
+edge_distance = 20
 
 
 root_windows: tkinter.Tk
 crawl_button: tkinter.Button
 import_xlsx_button: tkinter.Button
-
-pixel_virtual: tkinter.PhotoImage
 
 
 def render_ui():
@@ -20,21 +21,21 @@ def render_ui():
     global crawl_button
     global import_xlsx_button
 
-    global pixel_virtual
-
     root_windows = tkinter.Tk()
     root_windows.title("短视频播放量爬虫（0.0）")
-    root_windows.geometry("500x314")
-
-    pixel_virtual = tkinter.PhotoImage(width=1, height=1)
+    root_windows.geometry(f"{windows_width}x{windows_height}")
 
     crawl_button = tkinter.Button(root_windows,
                                   text="Begin crawl",
                                   height=button_height,
                                   width=button_width)
-    crawl_button.pack()
+    crawl_button.place(x=int(windows_width / 3 * 2), y=int(windows_height - edge_distance - button_height))
 
-
+    import_xlsx_button = tkinter.Button(root_windows,
+                                        text="Import form",
+                                        height=button_height,
+                                        width=button_width)
+    import_xlsx_button.place(x=int(windows_width / 3), y=int(windows_height - edge_distance - button_height))
 
 
     root_windows.mainloop()
