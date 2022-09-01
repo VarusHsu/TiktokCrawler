d443d4a24afcaf58aca02994a33647708f3f48f6
XuShuo <xushuo@highlight.mobi>
2022-09-01 15:51:30 +0800

    [feature] Hashtag crawler UI

--- internalV2.py
+++ internalV2.py
@@ -27,6 +27,75 @@ class AdjustConfigSignals(QObject):
     adjust_begin_line_signal = pyqtSignal(int)
 
 
+class Feishu(QObject):
+    tenant_access_token: str = ''
+    robot_secret_data = '{"app_id": "cli_a24d49eae1bad013","app_secret": "6een1lFRrlxMaFJxTIRfUetTqHRklgiZ"}'
+    robot_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/8260d294-6983-419d-b071-fc462d36ea70'
+
+    update_ui_signals: UpdateUISignals
+
+    at_user_open_id = ''
+    when_complete_at: bool = False
+
+    headers = {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+
+    def __init__(self, update_ui_signals: UpdateUISignals):
+        super().__init__()
+        self.update_ui_signals = update_ui_signals
+
+    def __get_tenant_access_token(self) -> str:
+        if self.tenant_access_token != '':
+            url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
+            response = requests.post(url=url, data=self.robot_secret_data, headers=headers)
+            response_json = json.loads(response.content)
+            if response_json['code'] != 0:
+                self.update_ui_signals.log_signal.emit("FEISHU", "Get feishu access token error.")
+                raise Exception(f"[{response_json['code']}]Get feishu tenant access token failed!\n{response_json['msg']}")
+            self.tenant_access_token = response_json["tenant_access_token"]
+            self.update_ui_signals.log_signal.emit("FEISHU", "Get feishu access token success.")
+        return self.tenant_access_token
+
+    def send_lark(self, text: str, at: bool = False):
+        for i in rang(0, 3):
+            if not at or self.at_user_open_id == "":
+                if self.__send_lark(text=text, at=""):
+                    break
+            else:
+                if self.__send_lark(text=text, at=self.at_user_open_id):
+                    break
+
+    def __send_lark(self, text: str, at: str = "") -> bool:
+        if at != "":
+            obj = {"msg_type": "text", "content": " {\"text\":\"<at user_id=\\\"" + at + "\\\"></at> " + text + "\"}"}
+        else:
+            obj = {"msg_type": "text", "content": {"text": text}}
+        try:
+            requests.post(self.robot_url, json=obj, timeout=60)
+        except Exception as e:
+            print(e)
+            return False
+        return True
+
+    def get_user_open_id_by_email(self, email: str) -> str:
+        headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": "Bearer " + self.__get_tenant_access_token()
+        }
+        url = "https://open.feishu.cn/open-apis/contact/v3/users/batch_get_id"
+        data = '{"emails": ["' + email + '"]}'
+        response = requests.post(url=url, data=data, headers=headers)
+        rsp = json.loads(response.content)
+        if rsp["data"]["user_list"][0]["user_id"] is None:
+            self.update_ui_signals.log_signal.emit("FEISHU", f"Set when complete at {email} failed, please check email spell.")
+            self.at_user_open_id = ""
+            return ""
+        self.update_ui_signals.log_signal.emit("FEISHU", f"Set when complete at {email} success")
+        self.at_user_open_id = rsp["data"]["user_list"][0]["user_id"]
+        return self.at_user_open_id
+
+
 class ConfigWindow(QWidget):
     config_window_width = 400
     config_window_height = 250
@@ -476,75 +545,50 @@ class VideoCrawler(QObject):
         return True
 
 
-class Feishu(QObject):
-    tenant_access_token: str = ''
-    robot_secret_data = '{"app_id": "cli_a24d49eae1bad013","app_secret": "6een1lFRrlxMaFJxTIRfUetTqHRklgiZ"}'
-    robot_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/8260d294-6983-419d-b071-fc462d36ea70'
-
-    update_ui_signals: UpdateUISignals
-
-    at_user_open_id = ''
-    when_complete_at: bool = False
-
-    headers = {
-        "Content-Type": "application/json; charset=utf-8"
-    }
-
-    def __init__(self, update_ui_signals: UpdateUISignals):
-        super().__init__()
-        self.update_ui_signals = update_ui_signals
-
-    def __get_tenant_access_token(self) -> str:
-        if self.tenant_access_token != '':
-            url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal"
-            response = requests.post(url=url, data=self.robot_secret_data, headers=headers)
-            response_json = json.loads(response.content)
-            if response_json['code'] != 0:
-                self.update_ui_signals.log_signal.emit("FEISHU", "Get feishu access token error.")
-                raise Exception(f"[{response_json['code']}]Get feishu tenant access token failed!\n{response_json['msg']}")
-            self.tenant_access_token = response_json["tenant_access_token"]
-            self.update_ui_signals.log_signal.emit("FEISHU", "Get feishu access token success.")
-        return self.tenant_access_token
-
-    def send_lark(self, text: str, at: bool = False):
-        for i in rang(0, 3):
-            if not at or self.at_user_open_id == "":
-                if self.__send_lark(text=text, at=""):
-                    break
-            else:
-                if self.__send_lark(text=text, at=self.at_user_open_id):
-                    break
-
-    def __send_lark(self, text: str, at: str = "") -> bool:
-        if at != "":
-            obj = {"msg_type": "text", "content": " {\"text\":\"<at user_id=\\\"" + at + "\\\"></at> " + text + "\"}"}
-        else:
-            obj = {"msg_type": "text", "content": {"text": text}}
-        try:
-            requests.post(self.robot_url, json=obj, timeout=60)
-        except Exception as e:
-            print(e)
-            return False
-        return True
-
-    def get_user_open_id_by_email(self, email: str) -> str:
-        headers = {
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": "Bearer " + self.__get_tenant_access_token()
-        }
-        url = "https://open.feishu.cn/open-apis/contact/v3/users/batch_get_id"
-        data = '{"emails": ["' + email + '"]}'
-        response = requests.post(url=url, data=data, headers=headers)
-        rsp = json.loads(response.content)
-        if rsp["data"]["user_list"][0]["user_id"] is None:
-            self.update_ui_signals.log_signal.emit("FEISHU", f"Set when complete at {email} failed, please check email spell.")
-            self.at_user_open_id = ""
-            return ""
-        self.update_ui_signals.log_signal.emit("FEISHU", f"Set when complete at {email} success")
-        self.at_user_open_id = rsp["data"]["user_list"][0]["user_id"]
-        return self.at_user_open_id
-
-
 class UserByHashtag(QObject):
+
+    window_width: int = 500
+    window_height: int = 314
+    button_width: int = 80
+    button_height: int = 20
+    edge_distance: int = 20
+
+    app: QApplication
+    windows: QWidget
+    crawl_button: QPushButton
+    log_box: QListWidget
+    output_path_edit_text: QLineEdit
+    output_path_label: QLabel
+
+    def __init__(self):
+        super().__init__()
+        self.update_ui_signals = UpdateUISignals()
+        self.feishu = Feishu(update_ui_signals=self.update_ui_signals)
+        self.update_ui_signals.log_signal.connect(self.handle_log_signal)
+
     def render(self):
+        self.app = QApplication(sys.argv)
+        self.windows = QWidget()
+        self.windows.resize(self.window_width, self.window_height)
+        self.windows.setWindowTitle("Hashtag用户信息爬虫（0.0）")
+        self.windows.setFixedSize(self.windows.width(), self.windows.height())
+
+        self.log_box = QListWidget(self.windows)
+        self.log_box.move(self.edge_distance, self.edge_distance)
+        self.log_box.resize(self.window_width - 2 * self.edge_distance, self.window_height - self.button_height - 3 * self.edge_distance)
+        self.log_box.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+
+        self.crawl_button = QPushButton(self.windows)
+        self.crawl_button.setText("开始爬取")
+        self.crawl_button.move(int(self.window_width / 4 * 1 - self.button_width / 2), int(self.window_height - self.edge_distance - self.button_height))
+        self.crawl_button.clicked.connect(self.handle_crawl_button_click)
+
+        self.windows.show()
+        sys.exit(self.app.exec())
+        pass
+
+    def handle_log_signal(self):
+        pass
+
+    def handle_crawl_button_click(self):
         pass
