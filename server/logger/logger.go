fa2dc97ae72ca586bd19bc9bbefb3b4974334fd0
XuShuo <xushuo@highlight.mobi>
2022-09-22 20:15:14 +0800

    [Feature] Add logger.

--- server/logger/logger.go
+++ server/logger/logger.go
@@ -1,1 +1,23 @@
 package logger
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const larkBotWebhookURL = "https://open.feishu.cn/open-apis/bot/v2/hook/61f8efec-d13b-47a6-8366-697b5db500ec"
+
+func LogMessage(logType string, logContent string) {
+	go func() {
+		msg := fmt.Sprintf("{\"msg_type\": \"text\", \"content\": {\"text\":\"[%s] %s\"}}", strings.ToLower(logType), logContent)
+		rsp, err := http.Post(larkBotWebhookURL, "application/json", strings.NewReader(msg))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(rsp.Status)
+
+	}()
+
+}
--- server/server.go
+++ server/server.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"draft/logger"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
-	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	r := gin.Default()
+	logger.LogMessage("START", "Crawl service start.")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
@@ -22,23 +24,32 @@ func main() {
 		file, headers, err := context.Request.FormFile("file")
 		if err != nil {
 			context.String(http.StatusBadRequest, "Bad request")
+			content := fmt.Sprintf("Post '%s' error, %s", "upload", err)
+			logger.LogMessage("ERROR", content)
 			return
 		}
 		filename := headers.Filename
 		out, err := os.Create(filename)
 		if err != nil {
-			log.Fatal("Error")
+			content := fmt.Sprintf("Create file error %s", err)
+			logger.LogMessage("ERROR", content)
 			return
 		}
 		defer out.Close()
 		_, err = io.Copy(out, file)
 		if err != nil {
+			content := fmt.Sprintf("Copy file error %s", err)
+			logger.LogMessage("ERROR", content)
 			return
 		}
+		content := fmt.Sprintf("Update xlsx success")
+		logger.LogMessage("UPDATE", content)
 		context.String(http.StatusCreated, "Upload Success")
 	})
 	err := r.Run()
 	if err != nil {
+		content := fmt.Sprintf("Gin run error.")
+		logger.LogMessage("ERROR", content)
 		return
 	}
 
