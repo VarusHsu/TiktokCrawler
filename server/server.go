2328a2de229ce7d7f7720c66e61cf100285e9d07
XuShuo <xushuo@highlight.mobi>
2022-09-22 19:20:46 +0800

    [Feature] Add File upload.

--- server/server.go
+++ server/server.go
@@ -1,19 +1,45 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 	r.GET("/test", func(context *gin.Context) {
 		context.File("../test.py")
 	})
+	r.POST("/upload", func(context *gin.Context) {
+		file, headers, err := context.Request.FormFile("file")
+		if err != nil {
+			context.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		filename := headers.Filename
+		out, err := os.Create(filename)
+		if err != nil {
+			log.Fatal("Error")
+			return
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			return
+		}
+		context.String(http.StatusCreated, "Upload Success")
+	})
 	err := r.Run()
 	if err != nil {
 		return
 	}
+
 }
